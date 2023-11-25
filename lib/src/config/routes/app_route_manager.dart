543da4e9284ac3ebe29590126d8d394570ae316a
ahmedtalal <ahmed.talal@gambit-projekt.com>
2023-11-25 23:57:07 +0200

    create routes for doctor app

---
+++ lib/src/config/routes/app_route_manager.dart
@@ -0,0 +1,30 @@
+import 'package:doctor_app/src/config/routes/app_routes_name.dart';
+import 'package:doctor_app/src/features/on_boarding/screens/on_boarding_screen.dart';
+import 'package:doctor_app/src/features/on_boarding/screens/splash_screen.dart';
+import 'package:get/route_manager.dart';
+
+class AppRouteManager {
+  static AppRouteManager? _appRouteManager;
+  AppRouteManager._internal();
+  static AppRouteManager instance() {
+    if (_appRouteManager == null) {
+      return _appRouteManager = AppRouteManager._internal();
+    }
+    return _appRouteManager!;
+  }
+
+  List<GetPage<dynamic>>? appGetPages = [
+
+    // splash screen //////
+    GetPage(
+      name: AppRouteNames.splashScreenRouteName,
+      page: () => const SplashScreen(),
+    ),
+
+    /// onboarding screen ///////
+    GetPage(
+      name: AppRouteNames.onBoardingRouteName,
+      page: () => const OnBoardingScreen(),
+    ),
+  ];
+}
---
+++ lib/src/config/routes/app_routes_name.dart
@@ -0,0 +1,4 @@
+class AppRouteNames{
+  static const String splashScreenRouteName = "/";
+  static const String onBoardingRouteName = "/onBoardingScreen";
+}
